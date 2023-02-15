5e2c34adeb01781bf7b31a1238ce4bd9b06f59fe
DarthByte <neverovm28@gmail.com>
2023-02-15 19:05:14 +0500

    Added model player and move this model.

--- map.py
+++ map.py
@@ -1,10 +1,9 @@
 import pygame
 
 import load_functions
-from errorsTypes import MapFormatError
-from tile import Tile
 from game_object import Tree
 from settings import SIZE_TILE
+from player import Player
 
 
 class Map:
@@ -27,24 +26,19 @@ class Map:
             column_result = []
 
             for x, item in enumerate(column):
-                if item not in self.tiles_types:
-                    raise MapFormatError(f"Invalid format map '{item}'")
-                column_result.append(
-                    Tile((x * SIZE_TILE, y * SIZE_TILE), load_functions.load_image("background", "green.png"),
-                         self.group_all_sprite))
 
                 if item == "#":
-                    Tree((x * SIZE_TILE, y * SIZE_TILE), self.group_all_sprite)
+                    Tree((x * SIZE_TILE, y * SIZE_TILE), self.group_all_sprite, self.group_obstacles_sprite)
+                elif item == "p":
+                    Player((x * SIZE_TILE, y * SIZE_TILE), load_functions.load_image("player", "starver.png"),
+                           self.group_obstacles_sprite, self.group_all_sprite)
 
             result.append(column_result)
 
         return result
 
     def run(self) -> None:
+        self.group_all_sprite.update()
+        self.screen.fill((1, 50, 32))
         for sprite in sorted(self.group_all_sprite):
             self.screen.blit(sprite.image, sprite.rect)
-
-
-if __name__ == '__main__':
-    map = Map("test_map.txt", {"#": 1, ".": 2, "p": 3})
-    print(map.array_tile)
--- player.py
+++ player.py
@@ -1,3 +1,68 @@
-class Player:
-    def __init__(self):
-        ...
+import pygame
+from constants import VERTICAL, HORIZONTAL
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos: tuple, image: pygame.Surface, obstacles_sprites: pygame.sprite.Group, *groups):
+        super().__init__(*groups)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacles_sprites = obstacles_sprites
+
+        self.main_score = 1000
+
+    def __lt__(self, other):
+        return self.main_score < other.main_score
+
+    def set_direction(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed: int):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * speed
+        self.collision(HORIZONTAL)
+        self.rect.y += self.direction.y * speed
+        self.collision(VERTICAL)
+
+    def collision(self, direction):
+        if direction == HORIZONTAL:
+            for sprite in self.obstacles_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+        if direction == VERTICAL:
+            for sprite in self.obstacles_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+
+    def update(self) -> None:
+        self.set_direction()
+        self.move(self.speed)
